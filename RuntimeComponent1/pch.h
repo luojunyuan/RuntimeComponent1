23d418981e056247624397360ba6702f773c4046
k1mlka <k1mlka@outlook.com>
2026-06-25 17:37:45 +0800

    添加项目文件。

---
+++ RuntimeComponent1/pch.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <windows.h>
+#include <unknwn.h>
+#include <restrictederrorinfo.h>
+#include <hstring.h>
+
+// Undefine GetCurrentTime macro to prevent
+// conflict with Storyboard::GetCurrentTime
+#undef GetCurrentTime
